cb649e8ba9c86dd547e520f297d46190994a55d4
aquadesk <aquadesk.cs@gmail.com>
2021-09-23 13:08:53 -0700

    fix react-router

--- src/server.tsx
+++ src/server.tsx
@@ -1,9 +1,10 @@
-import express from 'express';
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import { StaticRouter, StaticRouterContext } from 'react-router-dom';
+import express from "express";
+import React from "react";
+import { renderToString } from "react-dom/server";
+import { StaticRouter } from "react-router-dom";
+import { StaticRouterContext } from "react-router";
 
-import App from './App';
+import App from "./App";
 
 let assets: any;
 
@@ -13,17 +14,23 @@ const syncLoadAssets = () => {
 syncLoadAssets();
 
 const cssLinksFromAssets = (assets, entrypoint) => {
-  return assets[entrypoint] ? assets[entrypoint].css ?
-  assets[entrypoint].css.map(asset=>
-    `<link rel="stylesheet" href="${asset}">`
-  ).join('') : '' : '';
+  return assets[entrypoint]
+    ? assets[entrypoint].css
+      ? assets[entrypoint].css
+          .map((asset) => `<link rel="stylesheet" href="${asset}">`)
+          .join("")
+      : ""
+    : "";
 };
 
-const jsScriptTagsFromAssets = (assets, entrypoint, extra = '') => {
-  return assets[entrypoint] ? assets[entrypoint].js ?
-  assets[entrypoint].js.map(asset=>
-    `<script src="${asset}"${extra}></script>`
-  ).join('') : '' : '';
+const jsScriptTagsFromAssets = (assets, entrypoint, extra = "") => {
+  return assets[entrypoint]
+    ? assets[entrypoint].js
+      ? assets[entrypoint].js
+          .map((asset) => `<script src="${asset}"${extra}></script>`)
+          .join("")
+      : ""
+    : "";
 };
 
 export const renderApp = (req: express.Request, res: express.Response) => {
@@ -60,10 +67,10 @@ export const renderApp = (req: express.Request, res: express.Response) => {
 };
 
 const server = express()
-  .disable('x-powered-by')
+  .disable("x-powered-by")
   .use(express.static(process.env.RAZZLE_PUBLIC_DIR!))
-  .get('/*', (req: express.Request, res: express.Response) => {
-    const { html = '', redirect = false } = renderApp(req, res);
+  .get("/*", (req: express.Request, res: express.Response) => {
+    const { html = "", redirect = false } = renderApp(req, res);
     if (redirect) {
       res.redirect(redirect);
     } else {
